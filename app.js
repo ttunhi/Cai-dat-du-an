f0c64a21765c410abc65ab33aeeaa8e5160875ba
ttunhi <nhib1913255@student.ctu.edu.vn>
2022-09-23 22:11:49 +0700

    Đinh nghia cac route cho tai nguyen

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts".contactsRouter);
+app.use("/api/contacts",contactsRouter);
 
 app.get("/",(req,res)=>{
     res.json({ message: "Welcome to contact book application. "});
